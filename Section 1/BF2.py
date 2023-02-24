1d3eb57bb561dfcd36c25bfdb2bba4c54ade7f2e
Pranav Ghadge <57043249+pranavghadge@users.noreply.github.com>
2023-02-24 07:03:16 +0530

    Section 1 Commit

---
+++ Section 1/BF2.py
@@ -0,0 +1,8 @@
+# Bug-Fixing Exercise 2
+# The following code throws an error. Try to fix it.
+#
+# my_answer = input("What is your answer?")
+# answers = ['Yes', 'No', 'Yes' 'No' my_answer]
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes' 'No', my_answer]
---
+++ Section 1/CE1.py
@@ -0,0 +1,2 @@
+name = "Pranav"
+print(type(name))
